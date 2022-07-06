e8162698a8fa206866f989a4691a5f14d2b28fbc
Igor Flyunt <iflyunt@artelogic.net>
2022-07-06 15:43:54 +0300

    initial commit

---
+++ Vue JS 3.0 with .NET 5 Web API/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Vue_JS_3._0_with.NET_5_Web_API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
